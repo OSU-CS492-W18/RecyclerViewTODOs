3e6dfd0f522da8ed98e91b06a8c8580e1140da2d
Rob Hess <robwhess@gmail.com>
2018-01-18 16:26:16 -0800

    Add initial code from SimpleTODOs.

---
+++ app/src/main/java/com/example/android/recyclerviewtodos/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.recyclerviewtodos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayDeque;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTodoListTextView;
+    private EditText mTodoEntryEditText;
+
+    private ArrayDeque<String> mTodoList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTodoList = new ArrayDeque<String>();
+        mTodoListTextView = (TextView)findViewById(R.id.tv_todo_list);
+        mTodoEntryEditText = (EditText)findViewById(R.id.et_todo_entry);
+
+        Button addTodoButton = (Button)findViewById(R.id.btn_add_todo);
+        addTodoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoText = mTodoEntryEditText.getText().toString();
+                if (!TextUtils.isEmpty(todoText)) {
+                    mTodoList.push(todoText);
+                    mTodoListTextView.setText("");
+                    for (String todo : mTodoList) {
+                        mTodoListTextView.append(todo + "\n\n");
+                    }
+                    mTodoEntryEditText.setText("");
+                }
+            }
+        });
+
+    }
+}
